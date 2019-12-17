e7f12317a57a7c5af528ff8f99cc3df16d74bf73
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-18 04:28:39 +0700

    + navigation to show screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,23 +3,25 @@ import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from 'react-
 import {Context} from '../context/BlogContext'
 import {Feather} from '@expo/vector-icons'
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
     const {state, addBlogPost, deleteBlogPost} = useContext(Context)
     return (
-        <View>
-            <Button title="Add Post" onPress={addBlogPost} />
-            <FlatList 
-            data={state}
-            keyExtractor={(blogPost) => blogPost.title}
-            renderItem={({item}) => {
-                return <View style={styles.row}>
-                    <Text style={styles.title}>{item.title} - {item.id}</Text>
-                    <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                        <Feather style={styles.icon} name="trash" />
-                    </TouchableOpacity>
-                </View>
-            }} />
-        </View>
+        <TouchableOpacity onPress={()=> navigation.navigate('Show', {id: item.id})}>
+            <View>
+                <Button title="Add Post" onPress={addBlogPost} />
+                <FlatList 
+                data={state}
+                keyExtractor={(blogPost) => blogPost.title}
+                renderItem={({item}) => {
+                    return <View style={styles.row}>
+                        <Text style={styles.title}>{item.title} - {item.id}</Text>
+                        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                            <Feather style={styles.icon} name="trash" />
+                        </TouchableOpacity>
+                    </View>
+                }} />
+            </View>
+        </TouchableOpacity>
     )
 }
 
