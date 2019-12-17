29ba552bc85ce0f9c4d08339a86ca9c30b297d94
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-18 04:41:29 +0700

    ^ retrieving single post

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -6,22 +6,25 @@ import {Feather} from '@expo/vector-icons'
 const IndexScreen = ({navigation}) => {
     const {state, addBlogPost, deleteBlogPost} = useContext(Context)
     return (
-        <TouchableOpacity onPress={()=> navigation.navigate('Show', {id: item.id})}>
+        
             <View>
                 <Button title="Add Post" onPress={addBlogPost} />
                 <FlatList 
                 data={state}
                 keyExtractor={(blogPost) => blogPost.title}
                 renderItem={({item}) => {
-                    return <View style={styles.row}>
-                        <Text style={styles.title}>{item.title} - {item.id}</Text>
-                        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                            <Feather style={styles.icon} name="trash" />
-                        </TouchableOpacity>
-                    </View>
+                    return (
+                    <TouchableOpacity onPress={()=> navigation.navigate('Show', {id: item.id})}>
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title} - {item.id}</Text>
+                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                <Feather style={styles.icon} name="trash" />
+                            </TouchableOpacity>
+                        </View>
+                    </TouchableOpacity>
+                    )
                 }} />
             </View>
-        </TouchableOpacity>
     )
 }
 
