7580e0fbe2fecf759bdbfcb11ee0804f7878de3f
Azhari Muhammad M <azhari.marzan@gmail.com>
2019-12-19 13:18:56 +0700

    ^ delete post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,12 +13,6 @@ const blogReducer = (state, action) => {
             })
         case 'delete_blogpost' :
             return state.filter((blogPost) => blogPost.id !== action.payload)
-        case 'add_blogpost' :
-            return [...state, {
-                id: Math.floor(Math.random() * 9999), 
-                title: action.payload.title,
-                content: action.payload.content
-            }]
         default:
             return state
     }
@@ -35,7 +29,6 @@ const getBlogPosts = (dispatch) => {
 const addBlogPost = (dispatch) => {
     return async (title, content, callback) => {
         await jsonServer.post(`/blogposts`, {title, content});
-        // dispatch({type: 'add_blogpost', payload: {title, content}})
         if(callback) {
             callback()
         }
@@ -43,7 +36,8 @@ const addBlogPost = (dispatch) => {
 }
 
 const deleteBlogPost = (dispatch) => {
-    return (id) => {
+    return async (id) => {
+        await jsonServer.delete(`/blogposts/${id}`);
         dispatch({type: 'delete_blogpost', payload: id})
     }
 }
